fc729e481b4fafed6d704cca7244bb15afd2d577
ZEMIRLINE <y.zemirline@iia-formation.fr>
2022-11-17 12:23:09 +0100

    Ajout du service pour les catégories et setup de Eureka

---
+++ category-service/category-service/Controllers/Request/CategorieRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace category_service.Controllers.Request
+{
+    public class CategorieRequest
+    {
+        public string? Name { get; set; }
+    }
+}
---
+++ category-service/category-service/Startup/EurekaStartup.cs
@@ -0,0 +1,15 @@
+﻿using Steeltoe.Discovery.Client;
+using Steeltoe.Discovery.Eureka;
+
+namespace category_service.Startup
+{
+    public static class EurekaStartup
+    {
+        public static IServiceCollection UseEurekaConfiguration(this IServiceCollection services)
+        {
+            services.AddServiceDiscovery(options => options.UseEureka());
+
+            return services;
+        }
+    }
+}
