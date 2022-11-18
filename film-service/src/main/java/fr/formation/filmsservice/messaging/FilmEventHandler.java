bc13a7d0ca4074783f168f21e3ebcd4ffdfa91e4
DamienBuchet <damienbuchetdb@gmail.com>
2022-11-18 09:42:59 +0100

    Added RabbitMQ

---
+++ film-service/src/main/java/fr/formation/filmsservice/messaging/FilmEventHandler.java
@@ -0,0 +1,30 @@
+package fr.formation.filmsservice.messaging;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import fr.formation.filmsservice.model.Film;
+import fr.formation.filmsservice.repo.IFilmRepository;
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class FilmEventHandler {
+	private final IFilmRepository repoFilm;
+	
+	@Bean
+	public Consumer<FilmCreatedEvent> filmCreated(){
+		return evt -> {
+			Film film = Film.builder()
+					.id(evt.getFilmId())
+					.titre(evt.getTitre())
+					.description(evt.getDescription())
+					.prix(evt.getPrix())
+					.categories(evt.getCategorie())
+					.build();
+			this.repoFilm.save(film);
+		};
+	}
+}
