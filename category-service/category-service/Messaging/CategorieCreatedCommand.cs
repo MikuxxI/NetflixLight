d74e9fa91d7414f09cb8f7c09027a83fde80bb53
Corentin <corentin.jeannot2a@gmail.com>
2022-11-18 13:38:15 +0100

    Ajout du service pour les catégories et setup de RabbitMQ

--- category-service/category-service/Messaging/CategorieCreatedCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace category_service.Messaging
-{
-    public class CategorieCreatedCommand
-    {
-        public int CategorieId { get; set; }
-        public string CategorieName { get; set; } = "";
-    }
-}
--- category-service/category-service/Startup/RabbitMqStartup.cs
+++ category-service/category-service/Startup/RabbitMqStartup.cs
@@ -1,5 +1,5 @@
 ﻿using category_service.Messaging;
-using Steeltoe.CloudFoundry.Connector.RabbitMQ;
+using Steeltoe.Connector.RabbitMQ;
 using Steeltoe.Messaging.RabbitMQ.Config;
 using Steeltoe.Messaging.RabbitMQ.Extensions;
 
@@ -14,9 +14,19 @@ namespace category_service.Startup
             services.AddRabbitAdmin();
             services.AddRabbitTemplate();
 
+            // Configuration de la queue "produit en suppression" pour le service commentaire
+            services.AddRabbitQueue("ms.categorie.deletion");
+
             services.AddRabbitExchange("ms.categorie", ExchangeType.TOPIC);
 
-            services.AddRabbitQueue("ms.categorie.deletion");
+            // Liaison de cette queue à l'Exchange ms.produit
+            services.AddRabbitBinding("categorieDeletion", Binding.DestinationType.QUEUE, (p, b) => {
+                var binding = b as QueueBinding;
+
+                binding.Exchange = "ms.categorie";
+                binding.Destination = "ms.categorie.deletion";
+                binding.RoutingKey = "categorie.deletion.askfor"; // On écoute les demandes de suppression
+            });
 
             // Configuration du service qui captera les évènements reçus
             services.AddSingleton<CategorieEventHandler>();
