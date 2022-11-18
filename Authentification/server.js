fe33815dc512aba9dd7e7bfde20f1360c5ddeb0b
Corentin <corentin.jeannot2a@gmail.com>
2022-11-18 09:47:18 +0100

    Eureka Success

--- Authentification/server.js
+++ Authentification/server.js
@@ -11,69 +11,75 @@ const Eureka = require('eureka-js-client').Eureka;
 const client = new Eureka({
   // application instance information
   instance: {
-      app: 'authService',
-      instanceId: 'authServiceId',
-      port: {
-          '$': 8000,
-          '@enabled': 'true',
-      },
-      vipAddress: 'authService',
-      dataCenterInfo: {
-          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-          name: 'MyOwn',
-      },
-      registerWithEureka: true,
-      fetchRegistry: true 
+    app: 'AUTH-SERVICE',
+    instanceId: 'authServiceId',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port:  {
+        '$': 8000,
+        '@enabled': 'true',
+    },
+    vipAddress: 'AUTH-SERVICE',
+    statusPageUrl: 'http://localhost:8000/info',
+    dataCenterInfo:  {
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+        name: 'MyOwn',
+    },
+    registerWithEureka: true,
+    fetchRegistry: true
   },
   eureka: {
-      host: '10.111.20.244',
-      port: 9000
+      host: '10.111.21.78',
+      serviceUrls: 'http://10.111.21.78:9000/',
+      port: 9000,
+      servicePath: '/eureka/apps/'
   },
 });
-  
+
 client.logger.level('debug');
-// client.start((error) => {
-//         console.log(error || 'complete');
-// });
+  
+client.start((error) => {
+  console.log(error || 'complete');
+});
 
 //RABBITMQ
-const amqp = require("amqplib");
+// const amqp = require("amqplib");
 
-var channel, connection;  //global variables
-async function connectQueue() {   
-    try {
-        connection = await amqp.connect("amqp://10.111.20.244:5672");
-        channel    = await connection.createChannel()
+// var channel, connection;  //global variables
+// async function connectQueue() {   
+//     try {
+//         connection = await amqp.connect("amqp://10.111.21.78:5672");
+//         channel    = await connection.createChannel()
         
-        await channel.assertQueue("test-queue")
+//         await channel.assertQueue("test-queue")
         
-    } catch (error) {
-        console.log(error)
-    }
-}
-connectQueue();
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// connectQueue();
 
-async function sendData (data) {
-  // send data to queue
-  await channel.sendToQueue("Login-queue", Buffer.from(JSON.stringify(data)));
+// async function sendData (data) {
+//   // send data to queue
+//   await channel.sendToQueue("Login-queue", Buffer.from(JSON.stringify(data)));
       
-  // close the channel and connection
-  await channel.close();
-  await connection.close(); 
-}
+//   // close the channel and connection
+//   await channel.close();
+//   await connection.close(); 
+// }
 
-app.get("/api/auth/signin", (req, res) => {
+// app.get("/api/auth/signin", (req, res) => {
     
-  // data to be sent
-  const data = {
-      username  : req.body.username,
-      password : req.body.password
-  }
-  sendData(data);  // pass the data to the function we defined
-  console.log("A message is sent to queue")
-  res.send("Message Sent"); //response to the API request
+//   // data to be sent
+//   const data = {
+//       username  : req.body.username,
+//       password : req.body.password
+//   }
+//   sendData(data);  // pass the data to the function we defined
+//   console.log("A message is sent to queue")
+//   res.send("Message Sent"); //response to the API request
   
-})
+// })
 
 //Using App Express
 var corsOptions = {
